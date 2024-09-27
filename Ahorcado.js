58725ac1f1eed1fe92ef97b2ed610ec162268409
LoforteFran12 <d480670@alumnos.itr.edu.ar>
2024-09-27 08:58:57 -0300

    SimonDice, terminado

---
+++ Ahorcado.js
@@ -0,0 +1,14 @@
+const palabras = [
+    "amor", "paz", "felicidad", "familia", "amistad", "libertad", "esperanza", "sueño", "luz", "sonrisa",
+    "naturaleza", "cielo", "mar", "tierra", "vida", "corazón", "pasión", "sabiduría", "tiempo", "recuerdo",
+    "sabor", "arte", "música", "color", "cultura", "tradición", "historia", "aventura", "destino", "caminos",
+    "destello", "brillo", "mariposa", "estrella", "sonido", "silencio", "río", "montaña", "flor", "jardín",
+    "casa", "hogar", "amigo", "compañero", "viaje", "regalo", "sonido", "paso", "sombra", "lago",
+    "brisa", "nieve", "sol", "luna", "nube", "canto", "alegría", "tristeza", "esperanza", "ciencia",
+    "sabiduría", "estudio", "juego", "risas", "palabra", "pensamiento", "conocimiento", "misterio", "vida", "universo",
+    "pasado", "futuro", "momento", "creación", "ilusión", "realidad", "horizonte", "profundidad", "reflejo", "espejo",
+    "sueños", "destinos", "valores", "justicia", "honor", "valentía", "respeto", "generosidad", "empatía", "compasión",
+    "fuerza", "coraje", "poder", "sinceridad", "lealtad", "sabiduría", "misterios", "conexión", "descubrimiento", "aventura"
+];
+
+setup = () => {}
